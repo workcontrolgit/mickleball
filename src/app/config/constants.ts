b48d81d328585c4326c232cf3b9098607f3a321a
Fuji <fuji.nguyen@workcontrol.com>
2022-10-16 13:32:05 -0400

    Update scale discription

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -7,9 +7,9 @@ export class Constants {
   public readonly Api_Endpoint: string = environment.Api_Endpoint;
   public readonly Api_Mock_Endpoint: string = environment.Api_Mock_Endpoint;
   public readonly Ratings: { key: string; value: string }[] = [
-    { key: '0', value: '0 - Not observed or not able to execute' },
-    { key: '1', value: '1 - Attempted but very poorly executed/needs work' },
-    { key: '2', value: '2 - Good basic form, but needs work' },
-    { key: '3', value: '3 - Solid, consistent performance' },
+    { key: '0', value: 'Not observed or not able to execute' },
+    { key: '1', value: 'Attempted but very poorly executed/needs work' },
+    { key: '2', value: 'Good basic form, but needs work' },
+    { key: '3', value: 'Solid, consistent performance' },
   ];
 }
