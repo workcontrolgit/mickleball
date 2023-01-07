fc99ffe0358944259dac6f9d46352c4789cd3679
Fuji <fuji.nguyen@workcontrol.com>
2023-01-07 16:53:17 -0500

    update Youtube to YouTube

--- src/app/features/training/training.module.ts
+++ src/app/features/training/training.module.ts
@@ -6,7 +6,7 @@ import { TrainingRoutingModule } from './training-routing.module';
 import { TrainingComponent } from './training.component';
 import { SearchInputComponent } from './components/search-input/search-input.component';
 import { SearchListComponent } from './components/search-list/search-list.component';
-import { SearchContainerComponent } from './container/search-container/search-container.component';
+import { SearchContainerComponent } from './components/search-container/search-container.component';
 
 import { YouTubePlayerModule } from '@angular/youtube-player';
 
